d232cf52930bc47ee8fb87e6a36456f1d3d583d4
ikivanov <ikivanov@gmail.com>
2014-09-18 17:07:30 +0300

    node express framework added; ver 4.9.2

--- Server/OPCServer/app.js
+++ Server/OPCServer/app.js
@@ -1,9 +1,4 @@
-﻿
-/**
- * Module dependencies.
- */
-
-var express = require('express');
+﻿var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
@@ -11,27 +6,26 @@ var path = require('path');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(app.router); This is not needed anymore!!!
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+  //app.use(express.errorHandler());
+//}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('OPC server listening on port ' + app.get('port'));
 });
